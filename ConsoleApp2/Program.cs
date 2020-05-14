b9a1b089cba139fd01730013fd9cbd0c25ea44e2
Drizbize <62217928+Drizbize@users.noreply.github.com>
2020-05-14 21:05:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            if (number > 1)
+            {
+                while (i != number)
+                {
+                    Console.WriteLine(i++);
+                }
+                Console.WriteLine(i++);
+                Console.WriteLine("Done");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                Console.ReadKey();
+            }
+        }
+    }
+}
